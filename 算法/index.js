873d936f74accadf318f2e743adeddd4c37fa95a
jeffeliu <jeffeliu@tencent.com>
2022-06-22 20:17:52 +0800

    fix comment

--- 算法/index.js
+++ 算法/index.js
@@ -7,7 +7,7 @@
   for (let i = 0; i < len - 1; i++) {
     for (let j = 0; j < len - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        // 交换两者之间的顺序
+        // 双循环，交换两者之间的顺序
         let temp = arr[j + 1]
         arr[j + 1] = arr[j]
         arr[j] = temp
